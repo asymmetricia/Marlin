8dba7684e7d96642e037e5346a94cd586ed59f5b
Tricia Bogen <tricia-github@cernu.us>
2025-06-24 16:46:47 -0700

    set upload speed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -88,7 +88,7 @@
 
 // Choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_MELZI_MAKR3D
+  #define MOTHERBOARD BOARD_MELZI_V2
 #endif
 
 // @section serial
