d23afa2399e6977e3020bc0e84b737a620c8f2a0
Tricia Bogen <tricia-github@cernu.us>
2025-10-08 15:44:46 -0700

    increase max E feedrate

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1206,12 +1206,12 @@
  * Override with M203
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 300, 300, 4, 25 }
+#define DEFAULT_MAX_FEEDRATE          { 300, 300, 4, 100 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
-#if ENABLED(LIMITED_MAX_FR_EDITING)
-  #define MAX_FEEDRATE_EDIT_VALUES    { 600, 600, 10, 50 } // ...or, set your own edit limits
-#endif
+//#if ENABLED(LIMITED_MAX_FR_EDITING)
+//  #define MAX_FEEDRATE_EDIT_VALUES    { 600, 600, 10, 50 } // ...or, set your own edit limits
+//#endif
 
 /**
  * Default Max Acceleration (speed change with time) (linear=mm/(s^2), rotational=°/(s^2))
